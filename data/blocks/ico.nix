ff71f95e91d472b7718612b569d22d5df998e533
Stewart Mackenzie <setori88@gmail.com>
2017-07-13 02:02:11 +0800

    Problem: THE-TOKEN placeholder name is used and is confusing.
    Solution: use the token name instead

--- data/blocks/ico.nix
+++ data/blocks/ico.nix
@@ -4,13 +4,13 @@
   link = http://tokenmint.io;
   content = ''
     <b>Token Purpose:</b><p>
-    The Initial Coin Offering allows one to purchase THE-TOKEN which will be traded in the integrated marketplace.
+    The Initial Coin Offering allows one to purchase FRACTAL tokens which will be traded in the integrated marketplace.
     <p>
-    The buyer is someone who is prepared to offer THE-TOKEN for desired software available in the integrated marketplace that runs on Fractalide.
+    The buyer is someone who is prepared to offer FRACTALS for desired software available in the integrated marketplace that runs on Fractalide.
     <p>
-    The seller is someone who writes Fractalide compatible software and makes it available on the integrated marketplace and is prepared to accept THE-TOKEN for a sale.
+    The seller is someone who writes Fractalide compatible software and makes it available on the integrated marketplace and is prepared to accept FRACTALS for a sale.
     <p>
-    THE-TOKEN may also be used to contract programmers for writing custom components needed.
+    FRACTALS may also be used to contract programmers for writing custom components needed.
     <p>
     The buyer might not be technically minded but knows how to use Hypercard, and may choose to compose already purchased components forming new applications they may sell again. The original author of every component will always be compensated for every sale of a component hierarchy that includes their component.
     Hence people are rewarded for creating highly reusable, efficient components that solve real world problems.
