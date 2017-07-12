d539efdba15c706236687bbce56f52f5f2ee1087
Stewart Mackenzie <setori88@gmail.com>
2017-07-12 20:37:52 +0800

    Problem: header too complex
    Solution: keep it simple: "A Ethereum Classic Hypercard Client"

--- data/blocks/main-banner.nix
+++ data/blocks/main-banner.nix
@@ -2,7 +2,7 @@
   id = "main-banner";
   content = ''
     <div class="intro-text">
-      <div class="intro-lead-in">Hypercard extended into a new Ethereum based client.</div>
+      <div class="intro-lead-in">An Ethereum Classic Hypercard Client.</div>
       <a href="#ico" class="page-scroll btn btn-xl">Join ICO</a>
    </div>
   '';
