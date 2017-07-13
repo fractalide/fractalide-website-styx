b01701b06a3fcfcfc80ab01dc8925c0cbbed8344
Stewart Mackenzie <setori88@gmail.com>
2017-07-13 23:08:30 +0800

    Problem: ICO page too chaotic
    Solution: use unordered lists

--- data/blocks/ico.nix
+++ data/blocks/ico.nix
@@ -3,18 +3,29 @@
   title = "Join ICO";
   link = http://tokenmint.io;
   content = ''
-    <b>Token Purpose:</b><p>
-    The Initial Coin Offering allows one to purchase FRACTAL tokens which will be traded in the integrated marketplace.
-    <p>
-    The buyer is someone who is prepared to offer FRACTALS for desired software available in the integrated marketplace that runs on Fractalide.
-    <p>
-    The seller is someone who writes Fractalide compatible software and makes it available on the integrated marketplace and is prepared to accept FRACTALS for a sale.
-    <p>
-    FRACTALS may also be used to contract programmers for writing custom components needed.
-    <p>
-    The buyer might not be technically minded but knows how to use Hypercard, and may choose to compose already purchased components forming new applications they may sell again. The original author of every component will always be compensated for every sale of a component hierarchy that includes their component.
-    Hence people are rewarded for creating highly reusable, efficient components that solve real world problems.
-    <p>
+    <b>Token Name:</b> Fractal Coin<br>
+    <b>Token Symbol:</b> FRC<br>
+    <b>Token Purpose:</b><br>
+    <ul>
+      <li>
+        The Initial Coin Offering allows one to purchase FRACTAL coins which will be traded in the integrated marketplace.
+      </li>
+      <li>
+        A buyer is someone who is prepared to offer FRACTALS for desired software available in the integrated marketplace that runs on Fractalide.
+      </li>
+      <li>
+        A seller is someone who writes Fractalide compatible software and makes it available on the integrated marketplace and is prepared to accept FRACTALS in return.
+      </li>
+      <li>
+        FRACTALS may also be used to contract programmers to write custom components.
+      </li>
+      <li>
+        A component author will always be compensated upon every sale even if their component forms part of another programmer's component hierarchy.
+      </li>
+      <li>
+        Programmers write components, non-programmers may compose components and sell them in the integrated marketplace. This Rapid Application Development environment is designed for non-programmers to solve problems. So money doesn't just flow to programmers.
+      </li>
+    </ul>
     Please participate in the ICO over at Token Mint.
     <p>
     Note: Token Mint has not launched yet so the ICO isn't ready yet
