b220f0c351125922d00136dc2ee7515e0f21e1ce
Eric Sagnes <eric.sagnes@gmail.com>
2017-07-14 20:08:36 +0900

    Problem: Whitepaper template was containing data
    Solution: put the content back into data

--- data/eng/blocks/whitepaper.nix
+++ data/eng/blocks/whitepaper.nix
@@ -2,14 +2,32 @@
   id = "whitepaper";
   title = "Whitepaper";
 
-  text1 = ''
-    The one-pager describes the project in a succinct manner. It covers what has been done, the latest advancements after the Master Thesis implementation and what still needs to be done to reach our end goal.
-  '';
-  link1 = "/pdf/fractalideCOP.pdf";
-  implementation1 = "https://github.com/fractalide/fractalide";
-  text2 = ''
-    The entire master thesis describes the Hypercard implementation in detail. This is probably not for the casual investor due to the indepth technical descriptions.
-  '';
-  link2 = "/pdf/fractalideCOP.pdf";
-  implementation2 = "https://github.com/fractalide/fractalide-oz";
+  blocks = [{
+    text = ''
+      The one-pager describes the project in a succinct manner. It covers what has been done, the latest advancements after the Master Thesis implementation and what still needs to be done to reach our end goal.
+    '';
+    links = [{
+      to = "/pdf/fractalideCOP.pdf";
+      text = "Download One Pager";
+    } {
+      to = "https://github.com/fractalide/fractalide";
+      text = "Current implementation";
+      extra = ''
+        <img src="https://img.shields.io/github/stars/fractalide/fractalide.svg?style=flat-square">
+        <img src="https://img.shields.io/github/forks/fractalide/fractalide.svg?style=flat-square">
+        <img src="https://img.shields.io/badge/license-MPLv2-blue.svg?style=flat-square">
+      '';
+    }];
+  } {
+    text = ''
+      The entire master thesis describes the Hypercard implementation in detail. This is probably not for the casual investor due to the indepth technical descriptions.
+    '';
+    links = [{
+      to = "/pdf/fractalideCOP.pdf";
+      text = "Download Master Thesis";
+    } {
+      to = "https://github.com/fractalide/fractalide-oz";
+      text = "Original Master Thesis implementation";
+    }];
+  }];
 }
