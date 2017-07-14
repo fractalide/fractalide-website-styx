23e281418e6fa1a8aa010b5073305568980c193d
Stewart Mackenzie <setori88@gmail.com>
2017-07-14 19:41:38 +0800

    Problem: the brackets in main-banner cause a strange spacing problem
    Solution: use normal parenthesis

--- data/zho/blocks/main-banner.nix
+++ data/zho/blocks/main-banner.nix
@@ -2,7 +2,7 @@
   id = "main-banner";
   content = ''
     <div class="intro-text">
-      <div class="intro-lead-in">以太坊经典（ETC）HyperCard客户端应用市场</div>
+      <div class="intro-lead-in">以太坊经典(ETC)HyperCard客户端应用市场</div>
       <a href="#ico" class="page-scroll btn btn-xl">Join ICO</a>
    </div>
   '';
