51c12cbc387da95ca03587f4dbb126055d13a873
Stewart Mackenzie <setori88@gmail.com>
2017-07-16 21:39:04 +0800

    Problem: Many ICOs don't properly inform investors on risk.
    Solution: attempt to make sure investors have their eyes open.

--- data/eng/blocks/ico.nix
+++ data/eng/blocks/ico.nix
@@ -29,7 +29,43 @@
         Programmers write components, non-programmers may compose components and sell them in the integrated marketplace. This Rapid Application Development environment is designed for non-programmers to solve problems. So money doesn't just flow to programmers.
       </li>
     </ul>
-    Please participate in the ICO over at Token Mint.
+    <b>Not a Security:</b><br>
+    <ul>
+      <li>
+        Fractalide's ICO is <b>not</b> a security, you cannot earn dividends from your contribution.
+      </li>
+      <li>
+      The FRACTAL coin <b>does not</b> represent company equity.
+      </li>
+      <li>
+        This is a free and open source project, you will not be relying entirely on this organization and team to make FRACTALS worth more. It takes an entire community of people building software, sharing and contributing for this community and the FRACTAL coin to become valuable.
+      </li>
+      <li>
+        This project can be summed up nicely with this satement by Greg Medcraft, chairman of ASIC:
+<br><code>They're a very interesting concept. An ICO is not equity – you're offering basically something that is the product of the entity that is doing the launch. You're taking a bet on getting that product early. How different is that if I go to Kickstarter and I buy something – a watch – and then I get that watch and sell it in the future? It's no different, is it?</code>
+      </li>
+    </ul>
+    <b>Risks:</b><br>
+    Despite much of the research already being done and problem points solved, there are still risks associated with investing in this project.
+    Below are a list of the risks. We hope you can consider these risks and take appropriate measures.
+    The project can fail for a number of reasons:
+    <ul>
+      <li>
+        Should the FRACTAL coin not be listed on an exchange it will be difficult to see an increase in value.
+      </li>
+      <li>
+        Designing the fair compensation for contribution marketplace is a tricky problem. It could result in a number of different iterations and improvements for it to work well.
+      </li>
+      <li>
+        Whether or not people will enjoy programming and use it to solve real world problems en mass is also uncertain.
+      </li>
+    </ul>
+    <b>Invest</b><br>
+    Please participate in the ICO over at Token Mint as long as you follow these guidelines:
+    <ul>
+      <li>Don't invest more than you can afford to lose.</li>
+      <li>Don't invest in things you don't understand.</li>
+    </ul>
     <p>
     Note: Token Mint has not launched yet so the ICO isn't ready yet
   '';
