0763e75be8202080cf4c065f0445caf313985c2e
Stewart Mackenzie <setori88@gmail.com>
2017-07-16 19:34:41 +0800

    Problem: ETH in Chinese used in main-banner not ETC!
    Solution: swap ETH for ETC

--- data/zho/blocks/main-banner.nix
+++ data/zho/blocks/main-banner.nix
@@ -2,7 +2,7 @@
   id = "main-banner";
   content = ''
     <div class="intro-text">
-      <div class="intro-lead-in">具有App应用市场的以太坊(ETC)hypercard客户端</div>
+      <div class="intro-lead-in">具有App应用市场的以太坊经典(ETC)HyperCard客户端</div>
       <a href="#ico" class="page-scroll btn btn-xl">参与ICO</a>
    </div>
   '';
