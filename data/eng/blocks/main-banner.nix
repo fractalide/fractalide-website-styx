20b83c53e39101977f77ca3764cdc8ff770dc7f7
Stewart Mackenzie <setori88@gmail.com>
2017-07-16 16:20:26 +0800

    Problem: not explicitly clear about what kind of marketplace this is
    Solution: explicitly use the word App in both chinese and english

--- data/eng/blocks/main-banner.nix
+++ data/eng/blocks/main-banner.nix
@@ -2,7 +2,7 @@
   id = "main-banner";
   content = ''
     <div class="intro-text">
-      <div class="intro-lead-in">An Ethereum Classic Hypercard Client<br>with Integrated Marketplace.</div>
+      <div class="intro-lead-in">An Ethereum Classic Hypercard Client<br>with Integrated App Marketplace.</div>
       <a href="#ico" class="page-scroll btn btn-xl">Join ICO</a>
    </div>
   '';
