cee8f3e5cf8c943c81309b2c97996826f45f3f13
Stewart Mackenzie <setori88@gmail.com>
2017-07-16 16:05:26 +0800

    Problem: chinese main-banner not accurate translation
    Solution: convey the meaning without being too strange

--- data/zho/blocks/main-banner.nix
+++ data/zho/blocks/main-banner.nix
@@ -2,7 +2,7 @@
   id = "main-banner";
   content = ''
     <div class="intro-text">
-      <div class="intro-lead-in">以太坊经典(ETC)HyperCard客户端应用市场</div>
+      <div class="intro-lead-in">具有应用市场的以太坊(ETC)hypercard客户端</div>
       <a href="#ico" class="page-scroll btn btn-xl">参与ICO</a>
    </div>
   '';
