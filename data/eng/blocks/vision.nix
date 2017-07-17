5599acda337eb0c255ced8b476d0f3e7629924b2
Stewart Mackenzie <setori88@gmail.com>
2017-07-17 21:17:44 +0800

    Problem: concepts were conflated
    Solution: give names to each work unit

--- data/eng/blocks/vision.nix
+++ data/eng/blocks/vision.nix
@@ -2,30 +2,40 @@
   id = "vision";
   title = "Vision";
   content = ''
-    Fractalide is a Rapid Application Development (RAD) platform, an integrated app marketplace and a new Ethereum Classic client.
+    The vision is to create three foundational applications that run on an existing programming platform called Fractalide.
+    <ol>
+      <li><b>Hyperflow</b>: A Rapid Application Development (RAD) platform</li>
+      <li><b>Fractalmarket</b>: An integrated app marketplace</li>
+      <li><b>Etherflow</b>: A new Ethereum Classic client</li>
+    </ol>
 
-    <h5>Rapid Application Development platform</h5>
-    Fractalide draws much inspiration from <a href="https://en.wikipedia.org/wiki/HyperCard">HyperCard</a>. HyperCard was intuitive, semantically clear and easy to learn such that non-programmers could create powerful programs that solved real world problems at hand.
+    <b>Hyperflow</b> facilitates the creation of applications which can be published on <b>Fractalmarket</b>. <b>Etherflow</b> allows people to purchase applications on <b>Fractalmarket</b> or interact with other Ethereum Classic holders and contracts.
+
+    <h5>Hyperflow: Rapid Application Development platform</h5>
+    Hyperflow draws much inspiration from <a href="https://en.wikipedia.org/wiki/HyperCard">HyperCard</a>. HyperCard was intuitive, semantically clear and easy to learn such that non-programmers could create powerful programs that solved real world problems at hand.
     <br>HyperCard had two modes; an execution mode and development mode. Switching between the two modes was trivial and didn't require a compilation step. In develop mode, people built their Graphical User Interfaces (GUI) by dragging and dropping GUI widgets such as a button or textbox onto a canvas. This canvas was referred to as a Card. One could then write an English like programming language (called HyperTalk) to drive the execution of your Card GUI components and link Cards together. A set of Cards formed a Stack. The nearest analogy would be a Card is a web page and a Stack would be a website. People created Stacks which did anything from computer games to bookkeeping.
 
-    <br><br>Fractalide takes the good parts of HyperCard such as the simple intuitive development/execution environment that allows non-programmers to rapidly build applications, but chooses to reject certain shortcomings, namely not being able to share stacks over a network, and the English like language HyperTalk wasn't powerful enough for professional programmers.
+    <br><br>Hyperflow takes the good parts of HyperCard such as the simple intuitive development/execution environment that allows non-programmers to rapidly build applications, but chooses to reject certain shortcomings, namely not being able to share stacks over a network, and the English like language HyperTalk wasn't powerful enough for professional programmers.
 
     <br><br>We will swap out HyperTalk for a component oriented programming language called Flowscript (which is an implementation of a <a href="https://en.wikipedia.org/wiki/Flow-based_programming">Flow-based Programming language</a>). These components are typically written in modern day programming languages and are <b>designed</b> to be shareable, reusable and reproducible. The nearest analogy is Lego. Programmers create the lego blocks and non-programmers use the Lego blocks. So in this environment we can create a synergy between programmers and non-programmers. Programmers retain their custom powerful development environments and non-programmers can easily use their components to build powerful applications to solve real world problems.
 
     <br><br>Which leads us to the next point. We need a way to share these powerful components with the world!
 
-    <h5>Integrated App Marketplace</h5>
+    <h5>Fractalmarket: an Integrated App Marketplace</h5>
     Once the above HyperCard layer is completed applications can be built. A Marketplace app will be among the first few applications created. Very similar to how you have an Appstore app on your phone, this Fractalide Marketplace app allows you to search and pay for applications that run on Fractalide.
 
     <br><br>Though unlike the appstore, components that make up an application are shareable and may be included in other component hierachies. The original author is compensated for their work every time their component is sold even when it forms part of a component hierarchy created by another person.
 
     <br><br>These dynamics reward programmers for creating intuitive, reusable, efficient, well maintained components.
 
-    <br><br>The last notable point about the marketplace is that a new cryptocurrency will be created enabling trade between Fractalide buyers and sellers. This is the very same coin that can be purchased in the ICO.
+    <br><br>The last notable point about the marketplace is that a new cryptocurrency will be created enabling trade between buyers and sellers. This is the very same coin that can be purchased in the ICO.
 
-    <h5>A New Ethereum Classic Client</h5>
-    Another first comer app will be wrapping ETCDEV team's modular Ethereum Classic Emerald SDK libraries. This allows Fractalide to become an Ethereum Classic Client. Allowing for deep ETC blockchain functionality integration into the applications available on the Fractalide Marketplace.
+    <h5>Etherflow: A New Ethereum Classic Client</h5>
+    Etherflow builds on top of the work of ETCDEV by utilizing the modular Ethereum Classic Emerald SDK libraries. Etherflow aims to be a fully singing and dancing Ethereum Classic client. Allowing applications created by Hyperflow to perform transactions on the blockchain, interact with deployed cryptocontracts or just be wallet for the normal user.
     <br>
-    Programmers will be hired to assist ETCDEV to build out Emerald SDK.
+    Programmers will be hired to assist in the implementation of Emerald SDK and integration with Etherflow.
+    <br><br>
+    <h5>Future Work</h5>
+    Once the above requirements are implemented, future work on network overlays and building out libraries for the Internet of Things takes priority.
   '';
 }
