82b5a23fd98f6697662fee88b7f11d1a540f5f9e
Stewart Mackenzie <setori88@gmail.com>
2017-07-18 16:23:48 +0800

    Problem: ICO is too long and complex
    Solution: cut and slash

--- data/eng/blocks/ico.nix
+++ data/eng/blocks/ico.nix
@@ -11,59 +11,22 @@
     <b>Token Purpose:</b><br>
     <ul>
       <li>
-        The Initial Coin Offering allows one to purchase FRACTAL coins which will be traded in the integrated marketplace.
-      </li>
-      <li>
-        A buyer is someone who is prepared to offer FRACTALS for desired software available in the integrated marketplace that runs on Fractalide.
-      </li>
-      <li>
-        A seller is someone who writes Fractalide compatible software and makes it available on the integrated marketplace and is prepared to accept FRACTALS in return.
-      </li>
-      <li>
-        FRACTALS may also be used to contract programmers to write custom components.
-      </li>
-      <li>
-        A component author will always be compensated upon every sale even if their component forms part of another programmer's component hierarchy.
-      </li>
-      <li>
-        Programmers write components, non-programmers may compose components and sell them in the integrated marketplace. This Rapid Application Development environment is designed for non-programmers to solve problems. So money doesn't just flow to programmers.
+        The FRACTAL coin will be traded between component sellers and buyers in the <a href="#fractalmarket">Fractalmarket</a>. This coin has real utility and not just speculative in nature.
       </li>
     </ul>
-    <b>Not a Security:</b><br>
+    <b>Important note:</b><br>
     <ul>
       <li>
-        Fractalide's ICO is <b>not</b> a security, you cannot earn dividends from your contribution.
+        FRACTAL is <b>not</b> a security, you <b>cannot</b> earn dividends as FRACTAL does not represent company equity.
       </li>
       <li>
-      The FRACTAL coin <b>does not</b> represent company equity.
+        We will not immediately convert your contributions into Fiat currency thus creating an unstable market, indeed we aim to pay salaries in ETC.
       </li>
       <li>
-        This is a free and open source project, you will not be relying entirely on this organization and team to make FRACTALS worth more. It takes an entire community of people building software, sharing and contributing for this community and the FRACTAL coin to become valuable.
-      </li>
-      <li>
-        This project can be summed up nicely with this satement by Greg Medcraft, chairman of ASIC:
-<br><code>They're a very interesting concept. An ICO is not equity – you're offering basically something that is the product of the entity that is doing the launch. You're taking a bet on getting that product early. How different is that if I go to Kickstarter and I buy something – a watch – and then I get that watch and sell it in the future? It's no different, is it?</code>
+        It takes an entire open source community of people building, sharing, trading and contributing software for the FRACTAL coin to become valuable.
       </li>
     </ul>
-    <b>Risks:</b><br>
-    Despite much of the research already being done and problem points solved, there are still risks associated with investing in this project.
-    Below are a list of the risks. We hope you can consider these risks and take appropriate measures.
-    The project can fail for a number of reasons:
-    <ul>
-      <li>
-        Should the FRACTAL coin not be listed on an exchange it will be difficult to see an increase in value.
-      </li>
-      <li>
-        Designing the fair compensation for contribution marketplace is a tricky problem. It could result in a number of different iterations and improvements for it to work well.
-      </li>
-      <li>
-        Whether or not people will enjoy programming and use it to solve real world problems en mass is also uncertain.
-      </li>
-      <li>
-        As is the case in the ETH community, many ETH ICOs have converted their contributed ETH into Fiat currency as fast as possible which creates instablitiy in the cryptocurrency market. We have no intention of getting out of ETC (other than operational expenses) and are committed to this community for the long run. Indeed we aim for our programmers to be paid in ETC.
-      </li>
-    </ul>
-    <b>Invest</b><br>
+    <b>Invest:</b><br>
     Please participate in the ICO over at Token Mint as long as you follow these guidelines:
     <ul>
       <li>Don't invest more than you can afford to lose.</li>
