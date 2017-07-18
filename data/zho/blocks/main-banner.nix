9894ea9f89cef5e61eb0abc5a651c1f2ea9f4dcf
Stewart Mackenzie <setori88@gmail.com>
2017-07-18 18:12:27 +0800

    Problem: a traditional chinese character is used in the landing page
    Solution: change to traditional chinese

--- data/zho/blocks/main-banner.nix
+++ data/zho/blocks/main-banner.nix
@@ -2,7 +2,7 @@
   id = "main-banner";
   content = ''
     <div class="intro-text">
-      <div class="intro-lead-in">以太坊經典(ETC)基础设施ICO</div>
+      <div class="intro-lead-in">以太坊经典(ETC)基础设施ICO</div>
       <a href="#ico" class="page-scroll btn btn-xl">参与ICO</a>
    </div>
   '';
