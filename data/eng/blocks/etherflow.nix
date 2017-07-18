6fca144742fc230619ebe805b0c186d310538bcd
Stewart Mackenzie <setori88@gmail.com>
2017-07-18 21:51:24 +0800

    Problem: hyperflow, etherflow and fractalmarket description too complex
    Solution: strip it down massively

--- data/eng/blocks/etherflow.nix
+++ data/eng/blocks/etherflow.nix
@@ -3,8 +3,19 @@
   title = "Etherflow";
   subtitle = "A New Ethereum Classic Client";
   content = ''
-  Etherflow builds on top of the work of ETCDEV by utilizing the modular Ethereum Classic Emerald SDK libraries. Etherflow aims to be a fully singing and dancing Ethereum Classic client. Allowing applications created by Hyperflow to perform transactions on the blockchain, interact with deployed cryptocontracts or just be wallet for the normal user.
-  <br>
-  Programmers will be hired to assist in the implementation of Emerald SDK and integration with Etherflow.
+  Etherflow is a set of software components that'll heavily leverage ETCDEV's Emerald SDK.
+  <br><br>
+  We aim for these important results:
+  <ul>
+    <li>
+      Etherflow should present a convenient interface for users to interact with the ETC blockchain.
+    </li>
+    <li>
+    Reusable components will allow deep blockchain interaction with applications created by HyperFlow.
+    </li>
+    <li>
+      Long term collaboration with ETCDEV team is required to assist expanding out Emerald SDK's functionality and ensuring it operates in a stable expected fashion.
+    </li>
+  </ul>
     '';
 }
