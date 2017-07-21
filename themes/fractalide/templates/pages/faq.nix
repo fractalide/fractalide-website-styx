dc3c4ee9e90af9e05a8ea94ec7ccf4eef36c4e0e
Stewart Mackenzie <setori88@gmail.com>
2017-07-21 11:41:27 +0800

    Problem: FAQ bottom sentence cannot be seen on mobile platform
    Solution: add a few <br>

--- themes/fractalide/templates/pages/faq.nix
+++ themes/fractalide/templates/pages/faq.nix
@@ -4,14 +4,16 @@ normalTemplate (page: ''
   <div class="container">
   <h1>${page.title}</h1>
   <div class="faq">
-    ${mapTemplate (f: 
+    ${mapTemplate (f:
       templates.bootstrap.panel { heading = f.q; body = f.a; }
     ) page.items}
   </div>
   </div>
+  <br>
+  <br>
 '')
 /*
-    ${mapTemplate (f: 
+    ${mapTemplate (f:
       templates.bootstrap.panel { heading = f.q; body = f.a; }
     ) page.items}
 */
