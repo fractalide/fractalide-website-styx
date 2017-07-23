141b2e80aad158849ab29aebe198e3ea30003392
Stewart Mackenzie <setori88@gmail.com>
2017-07-24 01:37:30 +0800

    Problem: 2 pager ICO pdf document not present
    Solution: add it and point all relevant links to the document

--- data/eng/blocks/whitepaper.nix
+++ data/eng/blocks/whitepaper.nix
@@ -7,7 +7,7 @@ with lib;
   content = markdownToHtml ''
     The one-pager describes the project in a succinct manner. It covers what has been done, the latest advancements after the Master Thesis implementation and what still needs to be done to reach our end goal.
 
-    * ${templates.tag.ilink { to = "/pdf/fractalideCOP.pdf"; content = "Download One Pager"; }}
+    * ${templates.tag.ilink { to = "/pdf/Ethereum_Classic_Infrastructure_ICO.pdf"; content = "Download Two Pager"; }}
     * ${templates.tag.ilink { to = "https://github.com/fractalide/fractalide"; content = "Current implementation"; }}
       <img src="https://img.shields.io/github/stars/fractalide/fractalide.svg?style=flat-square">
       <img src="https://img.shields.io/github/forks/fractalide/fractalide.svg?style=flat-square">
