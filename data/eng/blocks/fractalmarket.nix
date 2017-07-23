f4308a275672a15d87469a2ac256ac083d257a6d
Stewart Mackenzie <setori88@gmail.com>
2017-07-24 07:40:46 +0800

    Problem: names sporadically change between camelCase and others
    Solution: standardize the names on Fractalmarket, Hyperflow
    and Etherflow

--- data/eng/blocks/fractalmarket.nix
+++ data/eng/blocks/fractalmarket.nix
@@ -5,12 +5,12 @@ with lib;
   title = "Fractalmarket";
   subtitle = "An Integrated App Marketplace";
   content = markdownToHtml ''
-    HyperFlow components and applications should be shared with people. This will be done via FractalMarket.
+    Hyperflow components and applications should be shared with people. This will be done via Fractalmarket.
 
-    FractalMarket has these features:
+    Fractalmarket has these features:
 
     * Users may buy and sell components and applications for FRACTAL.
     * Users may search and download components and applications.
-    * We are aligning FractalMarket to reward people for creating and using reusable components.
+    * We are aligning Fractalmarket to reward people for creating and using reusable components.
   '';
 }
