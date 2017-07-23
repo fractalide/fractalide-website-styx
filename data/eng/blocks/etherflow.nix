78902d06caf83360863e2ea94eac0e180dea8d99
Stewart Mackenzie <setori88@gmail.com>
2017-07-24 07:39:43 +0800

    Problem: not being clear about the ICO coverage
    Solution: explicitly mention that the ICO does not cover the
    funding of a new Ethereum Classic client

--- data/eng/blocks/etherflow.nix
+++ data/eng/blocks/etherflow.nix
@@ -1,16 +1,15 @@
-{ lib, ... }:
+{ lib, templates, ... }:
 with lib;
 {
   id = "etherflow";
   title = "Etherflow";
   subtitle = "A New Ethereum Classic Client";
   content = markdownToHtml ''
-    Etherflow is a set of software components that'll heavily leverage ETCDEV's Emerald SDK.
+    Etherflow is a set of reusable components that allows seamless Ethereum Classic blockchain interaction for applications created by ${templates.tag.ilink { to = "/#hyperflow"; content = "Hyperflow"; class = "page-scroll"; }}.
 
     We aim for these important results:
 
-    * Etherflow should present a convenient interface for users to interact with the ETC blockchain.
-    * Reusable components will allow deep blockchain interaction with applications created by HyperFlow.
-    * Long term collaboration with ETCDEV team is required to assist expanding out Emerald SDK's functionality and ensuring it operates in a stable expected fashion.
+    * Long term collaboration with ETCDEV team is required to assist expanding out Emerald SDK's functionality and ensuring it operates in a stable predictable fashion.
+    * Although the ICO doesn't cover a full blown ETC client, the general direction is to expand Etherflow out into a new ETC client.
   '';
 }
