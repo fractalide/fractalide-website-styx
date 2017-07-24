0f98ae9a5366d9ab0fcbdf3f83690e0f3052746d
Stewart Mackenzie <setori88@gmail.com>
2017-07-24 09:02:08 +0800

    Problem: typo, missed an `s' in stacks.
    Solution: add the `s'

--- data/eng/blocks/hyperflow.nix
+++ data/eng/blocks/hyperflow.nix
@@ -11,10 +11,10 @@ with lib;
 
     * The intuitive, semantically clear and easy to learn approach HyperCard is famous for.
     * It's very easy to see and test what you just built, by simply switching between a run mode and execution mode.
-    * Keep the concept of cards and stack, where a good analogy is a card is a webpage and a stack is a website. Each card contains GUI components and logic that links all the other cards together to form an stack of cards.
+    * Keep the concept of cards and stacks, a good analogy is a card is a webpage and a stack is a website. Each card contains GUI components and logic that links all the other cards together to form an stack of cards.
 
     HyperCard concepts to remove:
-    
+
     * Remove the HyperTalk programming language and replace it with Flowscript. HyperTalk isn't a powerful enough programming language for more serious apps.
     * Design it such that stacks can be shareable and reproducible across networks of computers.
   '';
