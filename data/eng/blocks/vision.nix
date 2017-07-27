dbc1aca8380f933b2dfad12bc9eaadef1232f85b
Stewart Mackenzie <setori88@gmail.com>
2017-07-28 03:14:43 +0800

    Problem: vision complex
    Solution: simplify further

--- data/eng/blocks/vision.nix
+++ data/eng/blocks/vision.nix
@@ -4,11 +4,12 @@ with lib;
   id = "vision";
   title = "Vision";
   content = markdownToHtml ''
-    It is unintuitive for non-technical end users to interact with published cryptocontracts. For this reason cryptocontracts will not easily reach mainstream.
+    Currently it is unintuitive for non-technical end users to interact with published cryptocontracts. For this reason cryptocontracts will not easily reach mainstream.
 
-    What is expected; a convenient one-click infrastructure that delivers beautiful 3rd party applications which are designed to use cryptocontracts. Making it easier for end users to interact with the blockchain.
+    End users expect a convenient one-click infrastructure that delivers beautiful yet secure 3rd party applications which are designed to use cryptocontracts.
+    This infrastructure can be broken down into three parts: building, distribution and the blockchain interaction parts.
 
-    The vision is to create three infrastructure level applications that enable the building, publishing and installation of the applications.
+    The vision is to build a platform that makes the building and distribution of blockchain integrated applications laughably easy.
 
     1. **${templates.tag.ilink { to = "/#hyperflow"; content = "Hyperflow"; class = "page-scroll"; }}**: A Rapid Application Development (RAD) platform
     2. **${templates.tag.ilink { to = "/#fractalmarket"; content = "Fractalmarket"; class = "page-scroll"; }}**: A cryptocontract app marketplace
