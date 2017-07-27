6bfaeab3f7bfbbafae1ae602acf7b1b08f2d3b6c
Stewart Mackenzie <setori88@gmail.com>
2017-07-27 15:20:57 +0800

    Problem: the vision isn't smooth
    Solution: try one more time to smooth it

--- data/eng/blocks/vision.nix
+++ data/eng/blocks/vision.nix
@@ -4,13 +4,14 @@ with lib;
   id = "vision";
   title = "Vision";
   content = markdownToHtml ''
-    Cryptocontracts will never reach mainstream as long as we keep publishing contracts to a blockchain address and expect users to interact with that address.
-    What is expected; convenient one-click infrastructure that delivers well designed user facing applications which contain the business rules to interact with the contract's immutable rules. The end user should barely see the cryptocontract address and primarily interact with the application.
+    It is not intuitive for non-technical end users to interact with published cryptocontracts. For this reason cryptocontracts will not easily reach mainstream.
 
-    The vision is to create three infrastructure level applications.
+    What is expected; a convenient one-click infrastructure that delivers well designed user facing applications which contain the business rules to interact with the contract's immutable rules. The end user should barely see the cryptocontract address and primarily interact with the application.
+
+    The vision is to create three infrastructure level applications enable the above.
 
     1. **${templates.tag.ilink { to = "/#hyperflow"; content = "Hyperflow"; class = "page-scroll"; }}**: A Rapid Application Development (RAD) platform
-    2. **${templates.tag.ilink { to = "/#fractalmarket"; content = "Fractamarket"; class = "page-scroll"; }}**: An integrated app marketplace
+    2. **${templates.tag.ilink { to = "/#fractalmarket"; content = "Fractamarket"; class = "page-scroll"; }}**: A cryptocontract app marketplace
     3. **${templates.tag.ilink { to = "/#etherflow"; content = "Etherflow"; class = "page-scroll"; }}**: A set of reusable components that make Hyperflow applications interacting with the blockchain seamless.
 
     **Hyperflow** facilitates the creation of applications which can be published on **Fractalmarket**. **Fractalmarket** aims to be the canonical repository for user facing cryptocontract applications created by third party vendors.
