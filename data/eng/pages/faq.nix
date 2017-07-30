fcbcc4354e04e2511be688d8a05aeb5000962e96
Stewart Mackenzie <setori88@gmail.com>
2017-07-30 23:14:54 +0800

    Problem: FAQ english was hardish to translate
    Solution: simplify it

--- data/eng/pages/faq.nix
+++ data/eng/pages/faq.nix
@@ -29,7 +29,7 @@
       Why do you need an ICO?
     '';
     a = ''
-      We need a currency for the marketplace, the currency should provide incentive to us to keep growing the value of the currency. The currency should also provide incentive to early backers to contribute to the project in many and varied ways.
+      We need a currency for the marketplace so users can buy and sell components.
     '';
   }
   {
@@ -37,12 +37,12 @@
       Will ETCDEV be compensated for their work on Emerald SDK?
     '';
     a = ''
-      We'll definitely assist ETCDEV to wrap Fractalide components around their software and help them publish on the app marketplace thus they will be compensated for their work. As will anyone else creating components.
-    '';
+      The blockchain components will be based on ETCDEV's libraries, as such they will earn money from purchases on Fractalmarket. This allows them to be sustainable and continue contributing to the Ethereum Classic community.
+      '';
   }
   {
     q = ''
-      ETC is in a unique position as it doesn't have a core development team with a large amount of premine/instamine, therefore it's expected that ICO parties on ETC should contribute towards the ETC core development. Do you intend on doing this?
+      ETC is in a unique position as it doesn't have a core development team with a large amount of pre-mine/instamine, therefore it's expected that ICO parties on ETC should contribute towards the ETC core development. Do you intend on doing this?
     '';
     a = ''
       Absolutely, we hope we'll be able to build and maintain a new ETC client from this. Also we will be working closely with ETCDEV team on developing the core blockchain libraries.
@@ -53,7 +53,7 @@
       Does the ICO build out a revenue generating company that'll be able to pay programmers in the long term?
     '';
     a = ''
-      Well, along with the premine which provides incentive to keep at the task the hopes are that enough revenue will be generated from the marketplace to build out a healthy company that hires enough programmers to maintain the infrastructure in a sustainable manner.
+      Well, along with the pre-mine which provides incentive to keep at the task the hopes are that enough revenue will be generated from the marketplace to build out a healthy company that hires enough programmers to maintain the infrastructure in a sustainable manner.
     '';
   }
   ];
