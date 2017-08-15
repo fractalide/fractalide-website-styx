d4e382e86dc9233ea51c234f075bc30f22822e5f
Stewart Mackenzie <setori88@gmail.com>
2017-08-15 13:58:19 +0800

    Problem: first sentence should describe what problem we're solving
    Solution: use "You don't have to be a programmer to use cryptocontracts"

--- data/eng/blocks/taster.nix
+++ data/eng/blocks/taster.nix
@@ -1,5 +1,6 @@
 {
   id = "taster";
-  subtitle = "
-  Finding apps on the Ethereum Classic (ETC) network is hard, using them is harder.<br>Fractalide is an app store much like Apple's and Google's.<br>A central repository where you can quickly find and use ETC's apps.";
+  subtitle = "You don't have to be a programmer to use cryptocontracts.<br><br>
+  Finding apps on the Ethereum Classic (ETC) network is hard, using them is harder.<br>Fractalide is an app store much like Apple's and Google's.<br>A central repository where you can quickly find and use ETC's apps.
+  ";
 }
