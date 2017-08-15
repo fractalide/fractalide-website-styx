dad26fd862d53d2388d5131f6767bb216b6bf9c6
Stewart Mackenzie <setori88@gmail.com>
2017-08-15 13:49:39 +0800

    Problem: readers will be confused by ETC without the meaning
    Solution: use '... Ethereum Classic (ETC)...' then continue using ETC

--- data/eng/blocks/taster.nix
+++ data/eng/blocks/taster.nix
@@ -1,5 +1,5 @@
 {
   id = "taster";
   subtitle = "
-  Finding apps on the ETC network is hard, using them is harder.<br>Fractalide is an app store much like Apple's and Google's.<br>A central repository where you can quickly find and use ETC's apps.";
+  Finding apps on the Ethereum Classic (ETC) network is hard, using them is harder.<br>Fractalide is an app store much like Apple's and Google's.<br>A central repository where you can quickly find and use ETC's apps.";
 }
