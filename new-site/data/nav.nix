c3815520b29baedb8e77fffe692ba95f669b240c
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-04-13 16:52:13 +0800

    Problem: no nav header
    Solution: Add menu definitions as data/nav.nix, port header.html to partials/nav.nix.

---
+++ new-site/data/nav.nix
@@ -0,0 +1,27 @@
+[
+  { name = "Home"; url = "/"; }
+  { name = "About us"; url = "/about_us/"; }
+  {
+    name = "Solutions";
+    children = [
+      { name = "Hyperflow"; url = "/hyperflow/"; }
+      { name = "Fractalmarket"; url = "/fractalmarket/"; }
+      { name = "Blockchain Construction"; url = "/blockchain_construction/"; }
+      { name = "Technical Analysis"; url = "/technical_analysis/"; }
+      { name = "Stake Pool"; url = "/stake_pool/"; }
+    ];
+  }
+  {
+    name = "Resources";
+    url = "/";
+    children = [
+      { name = "Research"; url = "/research/"; }
+      { name = "Documentation"; url = "/documentation/"; }
+      { name = "GitHub"; url = "https://github.com/fractalide/"; }
+      { name = "FAQs"; url = "/faqs/"; }
+    ];
+  }
+  { name = "Roadmap"; url = "/roadmap/"; }
+  { name = "Blog"; url = "/blog/"; }
+  { name = "Twitter"; pre = "twitter-min.png"; url = "http://twitter.com/fractalide"; }
+]
