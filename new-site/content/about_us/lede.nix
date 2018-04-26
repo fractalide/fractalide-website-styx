a27f8957e7027bec993be204d3a9f78d7da7f1ec
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-04-26 15:18:41 +0800

    Problem: no template for multi-section pages
    Solution: Implement page/sections.
    
    Apply it to about_us.

---
+++ new-site/content/about_us/lede.nix
@@ -0,0 +1,3 @@
+{ content = ''
+  Fractalide started life as a Master Thesis in Université catholique de Louvain under Professor Peter Van Roy back in 2014. Since then the project has undergone multiple rewrites in different programming languages, with new functionality and realizations. Every contribution helps us complete this undertaking. Thank you.
+''; }
