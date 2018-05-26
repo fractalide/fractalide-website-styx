744202e7a9b5f90a22cd85f8bda9abe67eca9ab3
Janice Leung Hayes <33276056+janiceleunghayes@users.noreply.github.com>
2018-05-26 12:29:55 +0900

    problem: grammar problems in about us lede

--- content/about_us/lede.nix
+++ content/about_us/lede.nix
@@ -1,3 +1,3 @@
 { content = ''
-  Fractalide started life as a Master Thesis in Université catholique de Louvain under Professor Peter Van Roy back in 2014. Since then the project has undergone multiple rewrites in different programming languages, with new functionality and realizations. Every contribution helps us complete this undertaking. Thank you.
+  Fractalide started life in 2014 as a Master Thesis in Université Catholique de Louvain under Professor Peter Van Roy. Since then, the project has undergone multiple rewrites in different programming languages, with new functionality and realizations. Every contribution helps us complete this undertaking, so thank you for your support.
 ''; }
