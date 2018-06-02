37f7bc07e7f3aeb46e7b9205ff083a9cb6bc7c9c
Stewart Mackenzie <setori88@gmail.com>
2018-06-02 14:25:25 +0800

    Problem: Janice and nixcloud not listed on team
    Solution: add them

---
+++ data/team/nixcloud.nix
@@ -0,0 +1,13 @@
+{
+"name" = "Nixcloud";
+"title" = "Operations";
+"image" = "photo-nixcloud-min.png";
+"weight" = 120;
+"networks" = [
+{
+"name" = "github";
+"url" = "https://github.com/nixcloud";
+"icon" = "icon-github-min.png";
+}
+];
+}
